dde0fc40a32a520add03c3b6ee9ed063fc7701d6
fann0128 <yangfanballack@gmail.com>
2017-09-18 18:05:36 +0000

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Fan
+                    Hello - Fan
                 </div>
 
                 <div class="links">
